0fc70429c2d49eaa329bb9bdef6ee66ecb9ccc29
NATAN BRENO FERREIRA <natan_b_ferreira@estudante.sesisenai.org.br>
2025-04-15 12:29:17 -0300

    nave

--- script.js
+++ script.js
@@ -10,4 +10,22 @@ const canvas = document.getElementById('JogoCanvas');
         this.color = color;
       }
 
+      desenhar() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
     }
+
+    class nave extends entidade {
+      constructor(x, y) {
+        super(x, y, 40, 20, 'lime');
+        this.speed = 5;
+      }
+
+      mover(dir) {
+        this.x += dir * this.speed;
+        this.x = Math.max(0, Math.min(canvas.width - this.width, this.x));
+      }
+    }
+
+    
